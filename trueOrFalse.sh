2cacb1b83ac4592db70eda7b7743873979f00a81
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2023-05-16 15:40:28 -0700

    redirect creds to login page

--- trueOrFalse.sh
+++ trueOrFalse.sh
@@ -2,4 +2,6 @@
 
 echo "Welcome to the True or False Game!"
 curl --silent --output ID_card.txt http://127.0.0.1:8000/download/file.txt
-cat ID_card.txt
+var=$(cat ID_card.txt | cut -d \" -f4,8 | tr \" :)
+curl --silent --output cookie.txt --cookie-jar cookie --user $var http://127.0.0.1:8000/login
+echo "Login message: " && cat cookie.txt
