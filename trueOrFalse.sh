667d144cc88a37c79c67e8e0bdb696bd9e428d90
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2023-05-16 18:28:17 -0700

    connect endpoint & return question

--- trueOrFalse.sh
+++ trueOrFalse.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Welcome to the True or False Game!"
+$var1 = "Login message: "
 curl --silent --output ID_card.txt http://127.0.0.1:8000/download/file.txt
 var=$(cat ID_card.txt | cut -d \" -f4,8 | tr \" :)
 curl --silent --output cookie.txt --cookie-jar cookie --user $var http://127.0.0.1:8000/login
 echo "Login message: " && cat cookie.txt
+curl --silent --output out.txt --cookie cookie http://127.0.0.1:8000/game
+echo "Response: " && cat out.txt
